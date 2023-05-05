a912325234cecf8adae0b47150edb23e8806364e
TJBaumgartner <tnorth6@gmail.com>
2023-05-05 13:19:42 -0700

    html skeleton created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './style.css';
-import printMe from './printMe.js';
-const btn = document.createElement('button');
-btn.innerHTML = 'Click me and check the console!';
-btn.onclick = printMe;
-
-document.body.appendChild(btn);
